380c75f6d13cc84a52c79eb208c9d21c9cc9879c
JamesTeague <jtteague13@gmail.com>
2019-12-24 18:04:49 -0600

    build: add git ignore, dependencies, and configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,17 @@
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
+import path from 'path';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/index.ts',
   output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-    },
+    { file: pkg.main, format: 'cjs', sourceMap: true },
+    { file: pkg.module, format: 'es', sourceMap: true },
   ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.devDependencies || {}),
-  ],
+  external: [...Object.keys(pkg.dependencies || {})],
   plugins: [
-    typescript({
-      typescript: require('typescript'),
-    }),
-  ],
+    typescript({ typescript: require('typescript') }),
+    commonjs(),
+  ]
 }
